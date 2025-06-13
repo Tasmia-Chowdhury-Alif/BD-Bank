249c3bcfa71f12795432cdcc7a21ca196b52aa2f
Tasmia-Chowdhury-Alif <tasmiachowdhuryalif222@gmail.com>
2025-06-13 22:13:18 +0600

    updated CSRF_TRUSTED_ORIGINS typing issue

--- Bangladesh_Bank/settings.py
+++ Bangladesh_Bank/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-CSRF_TRUSTED_ORIGINS = ['https://bd-bank-gqzo.onrender.com/', 'https://*.127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://bd-bank-gqzo.onrender.com', 'https://*.127.0.0.1']
 
 ALLOWED_HOSTS = ["*"]
 
